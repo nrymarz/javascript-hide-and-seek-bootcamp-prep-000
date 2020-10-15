17a22a2f2b1e1337709d403c99d7a33ae3f4e767
Nathan Rymarz <nrymarz@gmail.com>
2020-10-15 22:14:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.getElementsByClassName('target')[0]
 }
+function increaseRankBy(n) {
+  var rankedlist = document.getElementsByClassName('ranked-list').querySelectorAll('li')
+  for(let i=0; i<rankedlist.length; i++) {
+    rankedlist[i].innerHTML = (parseInt(rankedlist[i].innherHTML) + n).toString();
+  }
+}
