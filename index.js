5d4e42f2c1a96faf84e06669345d3ca535f0cde6
Nathan Rymarz <nrymarz@gmail.com>
2020-10-15 22:46:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.getElementsByClassName('target')[0]
+}
+function increaseRankBy(n) {
+  var rankedlist = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for(let i=0; i<rankedlist.length; i++) {
+    rankedlist[i].innerHTML = (parseInt(rankedlist[i].innerHTML) + n).toString();
+  }
+}
+function deepestChild() {
+  var current = document.getElementById('app').querySelector('#grand-node');
+  while(current.querySelector('div')) {
+    current = current.querySelector('div');
+  }
+  return current;
+}
